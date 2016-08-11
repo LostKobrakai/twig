f7360ce22d0c55eaa1133438f67af32105d4d25c
Benjamin Milde <benni@kobrakai.de>
2016-08-12 00:15:28 +0200

    Add frctl-twig as composer dependency

--- engine.js
+++ engine.js
@@ -11,7 +11,7 @@ exports.renderFile = function (entry, options, cb) {
   // Merge the global options with the local ones.
   options = Object.assign({}, twigOptions, options);
 
-  execPHP('php/Twig.php', null, function (error, php) {
+  execPHP('engine.php', null, function (error, php) {
     // Call the callback on error or the render function on success.
     error ? cb(error) : php.render(entry, options, function (error, stdout) {
       // Call the callback with an error or the trimmed output.
